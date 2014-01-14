2468a1f2dc5a3e1a442f54bcc3c48cec264ed622
minagaba <minagaba@gmail.com>
2014-01-14 17:40:06 +0200

    first

---
+++ Abotv1.1.1_Src/WebCrawlerWordsCount/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WebCrawlerWordsCount
+{
+    partial class WebCrawlerWordCount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siteURL = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.listViewResults = new System.Windows.Forms.ListView();
+            this.url = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.words = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filepath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.listBoxLogger = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxLinksFound = new System.Windows.Forms.TextBox();
+            this.textBoxLinksVisited = new System.Windows.Forms.TextBox();
+            this.textBoxLinksSkipped = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // siteURL
+            // 
+            this.siteURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.siteURL.Location = new System.Drawing.Point(72, 6);
+            this.siteURL.Name = "siteURL";
+            this.siteURL.Size = new System.Drawing.Size(312, 20);
+            this.siteURL.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goButton.Location = new System.Drawing.Point(390, 3);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(59, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(16, 32);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(498, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // listViewResults
+            // 
+            this.listViewResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.url,
+            this.words,
+            this.filepath});
+            this.listViewResults.FullRowSelect = true;
+            this.listViewResults.Location = new System.Drawing.Point(16, 94);
+            this.listViewResults.Name = "listViewResults";
+            this.listViewResults.Size = new System.Drawing.Size(498, 204);
+            this.listViewResults.TabIndex = 3;
+            this.listViewResults.UseCompatibleStateImageBehavior = false;
+            this.listViewResults.View = System.Windows.Forms.View.Details;
+            this.listViewResults.SelectedIndexChanged += new System.EventHandler(this.listViewResults_SelectedIndexChanged);
+            // 
+            // url
+            // 
+            this.url.Text = "url";
+            // 
+            // words
+            // 
+            this.words.Text = "words";
+            // 
+            // filepath
+            // 
+            this.filepath.Text = "File Path";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Site URL:";
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.settingsButton.Location = new System.Drawing.Point(455, 3);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(59, 23);
+            this.settingsButton.TabIndex = 5;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // listBoxLogger
+            // 
+            this.listBoxLogger.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxLogger.FormattingEnabled = true;
+            this.listBoxLogger.Location = new System.Drawing.Point(12, 317);
+            this.listBoxLogger.Name = "listBoxLogger";
+            this.listBoxLogger.Size = new System.Drawing.Size(498, 108);
+            this.listBoxLogger.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Log";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Links Found";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Links Visited";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Links Skipped";
+            // 
+            // textBoxLinksFound
+            // 
+            this.textBoxLinksFound.Location = new System.Drawing.Point(82, 62);
+            this.textBoxLinksFound.Name = "textBoxLinksFound";
+            this.textBoxLinksFound.ReadOnly = true;
+            this.textBoxLinksFound.Size = new System.Drawing.Size(47, 20);
+            this.textBoxLinksFound.TabIndex = 11;
+            // 
+            // textBoxLinksVisited
+            // 
+            this.textBoxLinksVisited.Location = new System.Drawing.Point(218, 62);
+            this.textBoxLinksVisited.Name = "textBoxLinksVisited";
+            this.textBoxLinksVisited.ReadOnly = true;
+            this.textBoxLinksVisited.Size = new System.Drawing.Size(47, 20);
+            this.textBoxLinksVisited.TabIndex = 12;
+            // 
+            // textBoxLinksSkipped
+            // 
+            this.textBoxLinksSkipped.Location = new System.Drawing.Point(363, 62);
+            this.textBoxLinksSkipped.Name = "textBoxLinksSkipped";
+            this.textBoxLinksSkipped.ReadOnly = true;
+            this.textBoxLinksSkipped.Size = new System.Drawing.Size(47, 20);
+            this.textBoxLinksSkipped.TabIndex = 13;
+            // 
+            // WebCrawlerWordCount
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(526, 437);
+            this.Controls.Add(this.textBoxLinksSkipped);
+            this.Controls.Add(this.textBoxLinksVisited);
+            this.Controls.Add(this.textBoxLinksFound);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxLogger);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewResults);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.siteURL);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "WebCrawlerWordCount";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox siteURL;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ListView listViewResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.ListBox listBoxLogger;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader url;
+        private System.Windows.Forms.ColumnHeader words;
+        private System.Windows.Forms.ColumnHeader filepath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxLinksFound;
+        private System.Windows.Forms.TextBox textBoxLinksVisited;
+        private System.Windows.Forms.TextBox textBoxLinksSkipped;
+    }
+}
+
---
+++ Abotv1.1.1_Src/WebCrawlerWordsCount/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Abot.Crawler;
+using Abot.Poco;
+
+namespace WebCrawlerWordsCount
+{
+    public partial class WebCrawlerWordCount : Form
+    {
+        delegate void StringParameterDelegate(string value);
+        delegate void CoutnersDelegate(int linksFound, int linksVisited, int linksSkipped);
+        delegate void UpdateListViewDelegate(string url, int wordsCount, string filepath);
+
+        int m_linksFound;
+        int m_linksVisited;
+        int m_linksSkipped;
+
+        string m_strSiteFolder;
+        string m_strSiteStatsFilename;
+
+        Uri m_baseURL;
+        BackgroundWorker m_backgroundWorker;
+
+        StreamWriter m_logFile;
+
+        public WebCrawlerWordCount()
+        {
+            InitializeComponent();
+            //siteURL.Text = @"http://www.ynet.co.il/";
+            //siteURL.Text = @"http://startupgrind.com/";
+            siteURL.Text = @"http://www.exent.com/";
+            progressBar.Minimum = 0;
+
+            m_linksFound = 0;
+            m_linksVisited = 0;
+            m_linksSkipped = 0;
+            UpdateCounters(m_linksFound, m_linksVisited, m_linksSkipped);
+
+            m_strSiteStatsFilename = "_stats.csv";
+
+            m_backgroundWorker = new BackgroundWorker();
+            m_backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorkerDoWork);
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(siteURL.Text, UriKind.Absolute))
+            {
+                CreateLogFile("log.txt");
+                m_baseURL = new Uri(siteURL.Text);
+                Log(@"Start checking site: " + m_baseURL.ToString());
+                Log("host is: " + m_baseURL.Host);
+                CreateFolderForSite(m_baseURL.Host);
+                
+                m_backgroundWorker.RunWorkerAsync(m_baseURL.ToString());
+            }
+            else
+            {
+                LogErr("site URL is malformed");
+            }
+
+        }
+
+        private void updateStartWorking()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(updateStartWorking));
+                return;
+            }
+            progressBar.Value = 0;
+            progressBar.Minimum = 0;
+            progressBar.Enabled = true;
+            listViewResults.Items.Clear();
+            siteURL.Enabled = false;
+            goButton.Enabled = false;
+            settingsButton.Enabled = false;
+        }
+
+        private void updateFinishedWorking()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(updateFinishedWorking));
+                return;
+            }
+            progressBar.Value = progressBar.Maximum;
+            progressBar.Enabled = false;
+            siteURL.Enabled = true;
+            goButton.Enabled = true;
+            settingsButton.Enabled = true;
+        }
+        
+        private void backgroundWorkerDoWork(object sender, DoWorkEventArgs e)
+        {
+            string url = (string)e.Argument;
+
+            updateStartWorking();
+            LogMsg(@"starting worker thread on url: " + url);
+
+            PoliteWebCrawler crawler = new PoliteWebCrawler();
+            crawler.PageCrawlStartingAsync += crawler_PageCrawlStartingAsync;
+            crawler.PageCrawlCompletedAsync += crawler_PageCrawlCompletedAsync;
+            crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowedAsync;
+            crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowedAsync;
+
+            Uri uri = new Uri(url);
+            CrawlResult result = crawler.Crawl(uri);
+
+            if (m_backgroundWorker.CancellationPending)
+            {
+                e.Cancel = true;
+                m_backgroundWorker.ReportProgress(0);
+            }
+
+            LogMsg("**** worker completed");
+            updateFinishedWorking();
+            listViewToCsvAsync();
+        }
+
+        void listViewToCsvAsync()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(listViewToCsvAsync));
+                return;
+            }
+            listViewToCsv(listViewResults, m_strSiteStatsFilename);
+        }
+
+        void listViewToCsv(ListView listView, string filename)
+        {
+            string filepath = Path.Combine(m_strSiteFolder, filename);
+            StreamWriter file = new StreamWriter(filepath);
+            string delimiter = ",";
+            file.WriteLine("statistics for site: " + siteURL.Text);
+            file.WriteLine("total links: " + listView.Items.Count.ToString());
+
+            // the data itself
+            foreach (ListViewItem item in listView.Items)
+            {
+                string line = "";
+                foreach (ListViewItem.ListViewSubItem subItem in item.SubItems)
+                {
+                    line += subItem.Text + delimiter;
+                }
+                // write line to file.
+                file.WriteLine(line);
+            }
+            file.Flush();
+            file.Close();
+        }
+
+        void crawler_PageCrawlStartingAsync(object sender, PageCrawlStartingArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            string msg;
+
+            m_linksFound++;
+            UpdateCounters(m_linksFound, -1, -1);
+
+            msg = "checking: " + pageToCrawl.Uri.AbsoluteUri.ToString() + " (parent: " + pageToCrawl.ParentUri.AbsoluteUri.ToString() + ")";
+            LogMsg(msg);
+        }
+
+        void crawler_PageCrawlCompletedAsync(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            string msg;
+
+            m_linksVisited++;
+            UpdateCounters(-1, m_linksVisited, -1);
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+            {
+                msg = "Failed to crawl to page: " + crawledPage.Uri.AbsoluteUri.ToString();
+                LogErr(msg);
+            } 
+            else
+            {
+                msg = "visited: " + crawledPage.Uri.AbsoluteUri.ToString();
+                LogMsg(msg);
+                CountWordsOnPage(crawledPage);
+            }
+        }
+
+        void crawler_PageCrawlDisallowedAsync(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            string msg;
+
+            m_linksSkipped++;
+            UpdateCounters(-1, -1, m_linksSkipped);
+
+            msg = "Did not crawl the links on page " + pageToCrawl.Uri.AbsoluteUri.ToString() + " due to " + e.DisallowedReason.ToString();
+            LogErr(msg);
+        }
+
+        void crawler_PageLinksCrawlDisallowedAsync(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            string msg;
+
+            m_linksSkipped++;
+            UpdateCounters(-1, -1, m_linksSkipped);
+
+            msg = "Did not crawl the links on page " + crawledPage.Uri.AbsoluteUri.ToString() + " due to " + e.DisallowedReason.ToString();
+            LogErr(msg);
+        }
+
+        private void CountWordsOnPage(CrawledPage crawledPage)
+        {
+            string url = crawledPage.Uri.AbsoluteUri.ToString();
+            HtmlAgilityPack.HtmlDocument htmlDoc = crawledPage.HtmlDocument;
+            HtmlAgilityPack.HtmlNode root = htmlDoc.DocumentNode;
+
+            string page_str = "";
+            foreach (HtmlAgilityPack.HtmlNode node in root.SelectNodes("//text()[not(parent::script)]"))
+            {
+                // scrap out unwanted nodes
+                if (node.ParentNode.Name == "script")
+                    continue;
+                if (node.ParentNode.Name == "style")
+                    continue;
+                if (node.ParentNode.Name == "#comment")
+                    continue;
+                string node_str = node.InnerText.Trim();
+                node_str = CleanupString(node_str);
+                page_str += "\n" + node_str;
+            }
+            string[] words = Regex.Split(page_str, @"\W+");
+            LogMsg("found " + words.Length + " words in " + url);
+
+            // create file name from url
+            string filename = crawledPage.Uri.AbsolutePath.Replace('/', '.');
+            // remove preceding and trailing '.'
+            filename = filename.Trim(new Char[] {' ', '.'});
+            filename += ".txt";
+            LogMsg("filename: " + filename);
+            SaveWords(filename, words);
+            UpdateListView(url, words.Length, filename);
+        }
+
+        private string CleanupString(string str)
+        {
+            string cleaned = str;
+            cleaned.Replace("&nbsp;", " ");
+            cleaned.Replace('.', ' ');
+            cleaned.Replace(',', ' ');
+            cleaned.Replace('\'', ' ');
+            cleaned.Replace('`', ' ');
+            return cleaned;
+        }
+
+        private void UpdateListView(string url, int wordsCount, string filepath)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new UpdateListViewDelegate(UpdateListView), url, wordsCount, filepath);
+                return;
+            }
+
+            ListViewItem item = new ListViewItem(url);
+            item.SubItems.Add(wordsCount.ToString());
+            item.SubItems.Add(filepath);
+
+            listViewResults.Items.Add(item);
+            listViewResults.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        private void UpdateCounters(int linksFound, int linksVisited, int linksSkipped)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new CoutnersDelegate(UpdateCounters), linksFound, linksVisited, linksSkipped);
+                return;
+            }
+
+            if (linksFound > -1)
+            {
+                textBoxLinksFound.Text = linksFound.ToString();
+                progressBar.Maximum = linksFound;
+            }
+            if (linksVisited > -1)
+            {
+                textBoxLinksVisited.Text = linksVisited.ToString();
+                progressBar.Value = linksVisited;
+            }
+            if (linksSkipped > -1)
+            {
+                textBoxLinksSkipped.Text = linksSkipped.ToString();
+            }
+        }
+
+        private void LogErr(string msg)
+        {
+            LogMsg("ERROR: " + msg);
+        }
+
+        private void LogMsg(string msg)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new StringParameterDelegate(LogMsg), msg);
+                return;
+            }
+            Log(msg);
+        }
+
+        private void Log(string msg)
+        {
+            string timestamp = DateTime.Now.ToString("[HH:mm:ss] ");
+            LogToFile(timestamp + msg);
+            listBoxLogger.Items.Add(timestamp + msg);
+            listBoxLogger.TopIndex = listBoxLogger.Items.Count - 1;
+        }
+
+        private void CreateLogFile(string filePath)
+        {
+            m_logFile = new StreamWriter(filePath,false, Encoding.UTF8);
+        }
+
+        private void LogToFile(string msg)
+        {
+            m_logFile.WriteLine(msg);
+        }
+
+        private void CloseLogFile()
+        {
+            m_logFile.Close();
+        }
+
+        private void CreateFolderForSite(string folderpath)
+        {
+            DirectoryInfo dirInfo = Directory.CreateDirectory(folderpath);
+            if (dirInfo.Exists)
+            {
+                m_strSiteFolder = folderpath;
+            }
+        }
+
+        private void SaveWords(string filename, string[] words)
+        {
+            string filepath = Path.Combine(m_strSiteFolder, filename);
+            LogMsg("saving words to file: " + filepath);
+            if (!File.Exists(filepath))
+            {
+                File.WriteAllLines(filepath, words, Encoding.UTF8);
+            }
+        }
+
+        private void listViewResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string row_str = "";
+            if (listViewResults.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listViewResults.SelectedItems[0];
+                for (int i = 0; i < item.SubItems.Count; i++)
+                {
+                    row_str += item.SubItems[i].Text + "\t";
+                }
+                Clipboard.SetText(row_str);
+            }
+        }
+
+    }
+}
